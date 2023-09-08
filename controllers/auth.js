b9e0844df4f5a1d1324df9ab6782feb765a18ede
Spich <spichakovda@gmail.com>
2023-09-09 01:56:42 +0300

    hash password, create jwt token

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,25 +3,32 @@
 const User = require("../models/User")
 const { StatusCodes } = require("http-status-codes")
 const { BadRequestError } = require("../errors")
-const bcrypt = require("bcryptjs")
-
-
+// const bcrypt = require("bcryptjs")
+const jwt = require("jsonwebtoken")
 
 
 const register = async (req, res) => {
     // Example of custom error
-    const { name, email, password } = req.body
-    if (!name || !email || !password) {
-        throw new BadRequestError("Please provide name, email and password")
-    }
+    // const { name, email, password } = req.body
+    // if (!name || !email || !password) {
+    //     throw new BadRequestError("Please provide name, email and password")
+    // }
     //
-    const salt = await bcrypt.genSalt(10)
-    const hashPassword = await bcrypt.hash(password, salt)
+    // const salt = await bcrypt.genSalt(10)
+    // const hashPassword = await bcrypt.hash(password, salt)
 
-    const tempUser = { name, email, password: hashPassword }
+    // const tempUser = { name, email, password: hashPassword }
 
-    const user = await User.create({ ...tempUser })
-    res.status(StatusCodes.CREATED).json(user)
+    // const user = await User.create({ ...tempUser })
+
+    const user = await User.create({ ...req.body })
+
+    // const token = jwt.sign({ userId: user._id, name: user.name }, "jwtSecret", { expiresIn: "30d" })
+    const token = user.createJWT()
+
+    res
+        .status(StatusCodes.CREATED)
+        .json({ user: { name: user.name }, token })
 }
 
 const login = async (req, res) => {
