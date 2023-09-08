789b7cba83a73593819ec6f1ac01b1b8931a9164
Spich <spichakovda@gmail.com>
2023-09-08 06:36:59 +0300

    hash password

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,10 +2,25 @@
 
 const User = require("../models/User")
 const { StatusCodes } = require("http-status-codes")
+const { BadRequestError } = require("../errors")
+const bcrypt = require("bcryptjs")
+
+
 
 
 const register = async (req, res) => {
-    const user = await User.create({ ...req.body })
+    // Example of custom error
+    const { name, email, password } = req.body
+    if (!name || !email || !password) {
+        throw new BadRequestError("Please provide name, email and password")
+    }
+    //
+    const salt = await bcrypt.genSalt(10)
+    const hashPassword = await bcrypt.hash(password, salt)
+
+    const tempUser = { name, email, password: hashPassword }
+
+    const user = await User.create({ ...tempUser })
     res.status(StatusCodes.CREATED).json(user)
 }
 
